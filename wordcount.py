33004020d0d6e20fd009dd03d030eda564480195
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:48:52 -0700

    Built count function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,17 @@
 # put your code here.
+def counts_word(file):
+    """DocString: Function that counts the words in file test.txt"""
+    file = open(file)
+    words_count = {}
+
+    for line in file:
+        line = line.rstrip()
+        line = line.split()
+
+        for word in line:
+            words_count[word] = words_count.get(word, 0) + 1
+
+    for word, number in words_count.items():
+        print('{} : {}'.format(word, number))
+
+counts_word("test.txt")
